cc279ca4a10087dddaa0a4771ee509190cdec20a
yuancw74 <100943173+yuancw74@users.noreply.github.com>
2022-05-01 06:07:56 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -15,14 +15,15 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '$a src-git lede https://github.com/coolsnowwolf/luci' feeds.conf.default
+# sed -i '$a src-git lede https://github.com/coolsnowwolf/luci' feeds.conf.default
 # sed -i '$a src-git immpackage https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
-sed -i '$a src-git immortalwrt https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
+# sed -i '$a src-git immortalwrt https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
 # sed -i '$a src-git ledepackage https://github.com/coolsnowwolf/packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+
 
  
 # Define My Package
