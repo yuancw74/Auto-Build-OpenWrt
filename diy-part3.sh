17a14553307db4072b9dd4fd26246b2fd2979619
yuancw74 <100943173+yuancw74@users.noreply.github.com>
2022-05-08 06:44:22 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -17,13 +17,14 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git lede https://github.com/coolsnowwolf/luci' feeds.conf.default
 git clone  https://github.com/KFERMercer/luci-app-baidupcs-web package/luci-app-baidupcs-web
-sed -i '$a src-git imm https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
+# sed -i '$a src-git imm https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.defaul
+
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 
  
