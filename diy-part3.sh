6b326f688861f10849ec480340e7bfa3dcefafab
yuancw74 <100943173+yuancw74@users.noreply.github.com>
2022-05-12 05:34:05 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git lede https://github.com/coolsnowwolf/luci' feeds.conf.default
-git clone https://github.com/KFERMercer/luci-app-baidupcs-web package/luci-app-baidupcs-web
-git clone https://github.com/jacob2826/luci-app-turboacc package/turboacc
+# git clone https://github.com/KFERMercer/luci-app-baidupcs-web package/luci-app-baidupcs-web
+# git clone https://github.com/jacob2826/luci-app-turboacc package/turboacc
 git clone https://github.com/sbwml/openwrt-alist package/openwrt-alist
 sed -i '$a src-git imm https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.defaul
